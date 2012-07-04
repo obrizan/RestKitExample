26a6c5e267491e00d2f6f2b21d600cf4ae19ec2f
Vladimir Obrizan <volodymyr.obrizan@dnt-lab.com>
2012-07-04 13:10:38 +0300

    Initial Commit

---
+++ RestKitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RestKitExample
+//
+//  Created by Volodymyr Obrizan on 04.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
